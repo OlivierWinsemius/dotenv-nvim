fa51fa78c40ee45682a03aa16f69afc8d16b2b00
Olivier Winsemius <olivierwinsemius@Mac.home>
2025-01-10 12:14:47 +0100

    feat: removing unused packages

--- after/plugin/vim-test.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set("n", "<leader>t", vim.cmd.TestNearest);
-vim.keymap.set("n", "<leader>T", vim.cmd.TestFile);
-vim.keymap.set("n", "<leader>a", vim.cmd.TestSuite);
-vim.keymap.set("n", "<leader>l", vim.cmd.TestLast);
-vim.keymap.set("n", "<leader>g", vim.cmd.TestNearest);
-
-vim.cmd("let test#strategy = 'vimux'");
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -21,7 +21,6 @@ return {
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
     {
-
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
     },
@@ -65,75 +64,18 @@ return {
     { "mbbill/undotree" },
     { "tpope/vim-fugitive" },
     { "sindrets/diffview.nvim" },
-    -- { "sunjon/shade.nvim" },
-    -- lsp-zero
-    { 'VonHeikemen/lsp-zero.nvim',            branch = 'v4.x' },
+
     { 'neovim/nvim-lspconfig' },
-    { 'hrsh7th/cmp-nvim-lsp' },
     { 'hrsh7th/nvim-cmp' },
-
     { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/cmp-buffer' },
-    { 'hrsh7th/cmp-path' },
-    { 'hrsh7th/cmp-cmdline' },
-    { 'hrsh7th/nvim-cmp' },
-    { 'hrsh7th/vscode-langservers-extracted' },
-
-    { 'L3MON4D3/LuaSnip' },
-    { 'saadparwaiz1/cmp_luasnip' },
-    -- lsp-zero end
 
     { 'github/copilot.vim' },
     { "alexghergh/nvim-tmux-navigation" },
     { "lewis6991/gitsigns.nvim" },
     {
         'stevearc/oil.nvim',
-        ---@module 'oil'
-        ---@type oil.SetupOpts
         opts = {},
-        -- Optional dependencies
         dependencies = { { "echasnovski/mini.icons", opts = {} } },
         -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-    },
-    { 'edluffy/hologram.nvim' },
-    {
-        "vim-test/vim-test",
-        dependencies = {
-            "preservim/vimux",
-        },
-    },
-    {
-        "epwalsh/obsidian.nvim",
-        version = "*", -- recommended, use latest release instead of latest commit
-        lazy = true,
-        ft = "markdown",
-        -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-        -- event = {
-        --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-        --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-        --   -- refer to `:h file-pattern` for more examples
-        --   "BufReadPre path/to/my-vault/*.md",
-        --   "BufNewFile path/to/my-vault/*.md",
-        -- },
-        dependencies = {
-            -- Required.
-            "nvim-lua/plenary.nvim",
-
-            -- see below for full list of optional dependencies 👇
-        },
-        opts = {
-            workspaces = {
-                {
-                    name = "personal",
-                    path = "~/vaults/personal",
-                },
-                {
-                    name = "work",
-                    path = "~/vaults/work",
-                },
-            },
-
-            -- see below for full list of options 👇
-        },
-    },
+    }
 }
